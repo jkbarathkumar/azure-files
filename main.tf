519c89191ede8045422fa2cb55943a6f26449ece
Barathkumar <140275835+jkbarathkumar@users.noreply.github.com>
2025-05-09 12:15:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+
 terraform {
   required_providers {
     azurerm = {
@@ -5,10 +6,10 @@ terraform {
       version = "~>3.0"
     }
   }
-   backend "azurerm" {
-      resource_group_name  = "barathbackend"
-      storage_account_name = "tfstatejbnmh"
-      container_name       = "tfstate"
+  backend "azurerm" {
+      resource_group_name  = "dev-tfstate"
+      storage_account_name = "devbackendtfstatehdwua"
+      container_name       = "dev-tfstate-blob"
       key                  = "terraform.tfstate"
   }
 
@@ -18,6 +19,7 @@ provider "azurerm" {
   features {}
 }
 
+
 resource "random_string" "resource_code" {
   length  = 5
   special = false
@@ -25,12 +27,12 @@ resource "random_string" "resource_code" {
 }
 
 resource "azurerm_resource_group" "tfstate" {
-  name     = "barathbackend"
-  location = "East US"
+  name     = "dev-tfstate"
+  location = "central india"
 }
 
 resource "azurerm_storage_account" "tfstate" {
-  name                     = "tfstate${random_string.resource_code.result}"
+  name                     = "devbackendtfstate${random_string.resource_code.result}"
   resource_group_name      = azurerm_resource_group.tfstate.name
   location                 = azurerm_resource_group.tfstate.location
   account_tier             = "Standard"
@@ -43,13 +45,81 @@ resource "azurerm_storage_account" "tfstate" {
 }
 
 resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
+  name                  = "dev-tfstate-blob"
   storage_account_name  = azurerm_storage_account.tfstate.name
   container_access_type = "private"
 }
 
 
-resource "azurerm_resource_group" "tfstate3" {
-  name     = "barathtesting-1"
-  location = "East US"
+
+resource "azurerm_resource_group" "example" {
+  name     = "barath-demo"
+  location = "centralindia"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "barath-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+ 
+resource "azurerm_subnet" "example" {
+  name                 = "barath-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+ 
+resource "azurerm_public_ip" "public_ip" {
+  name                = "barath-public-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method = "Static"
+}
+ 
+resource "azurerm_network_interface" "example" {
+  name                = "barath-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+ 
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+ 
+ 
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "barath-machine"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  size                = "Standard_F2"
+  admin_username      = "User15"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+ 
+  admin_ssh_key {
+    username   = "User15"
+    public_key = file("/home/user15/.ssh/id_rsa.pub")
+  }
+ 
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+ 
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+ 
+  tags = {
+    Name = "dev"
+  }
 }
